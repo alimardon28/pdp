dc267ecb5d145b3854bbc0b78b3946f3f806f63d
alimardon28 <doniyorovalimardon369@gmail.com>
2022-08-27 23:53:18 +0500

    ok

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,69 @@
-import React from 'react';
+import React from "react";
 import "../Header/Header.scss";
+import icons from "../../assets/icons/icons.svg";
+import enter from "../../assets/icons/enter.svg";
+import menu from "../../assets/pdpicons/pdpuz-menu.svg";
+import menu1 from "../../assets/pdpicons/pdpuz-menu1.svg";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-        <div className='header'>
+  const [active, setActive] = useState(false);
 
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <div className="container">
+        <nav className="header__nav">
+          <a className="header__nav_link" href="#">
+            <img className="header__nav_link_img" src={icons} alt="" />
+          </a>
+          {active && (
+            <ul className="header__nav_ul">
+              <li className="header__nav_ul_li">
+                <a
+                  className="header__nav_ul_li_a header__nav_ul_li_links"
+                  href="#"
+                >
+                  <img src={menu} alt="" />
+                  kurslarimiz
+                </a>
+              </li>
+              <li className="header__nav_ul_li">
+                <a
+                  className="header__nav_ul_li_a header__nav_ul_li_link"
+                  href="#"
+                >
+                  qo'llanma
+                  <span></span>
+                </a>
+                <a
+                  className="header__nav_ul_li_a header__nav_ul_li_link"
+                  href="#"
+                >
+                  unicorn
+                  <span></span>
+                </a>
+              </li>
+              <li className="header__nav_ul_li">
+                <a
+                  className="header__nav_ul_li_a header__nav_ul_li_links"
+                  href="#"
+                >
+                  <img src={enter} alt="" />
+                  kirish
+                </a>
+              </li>
+            </ul>
+          )}
+          <button
+            className="header__nav_button"
+            onClick={(e) => setActive(!active)}
+          >
+            <img src={menu1} alt="" />
+          </button>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
