fb95cf39fba9fc943ced54c188632d88303b289f
alimardon28 <doniyorovalimardon369@gmail.com>
2022-08-29 22:30:44 +0500

    oko

---
+++ src/API/approach.jsx
@@ -0,0 +1,45 @@
+import edu5 from "../assets/edupdp/edu5.svg";
+import edu6 from "../assets/edupdp/edu6.svg";
+import edu7 from "../assets/edupdp/edu7.svg";
+import edu8 from "../assets/edupdp/edu8.svg";
+import edu9 from "../assets/edupdp/edu9.svg";
+import edu10 from "../assets/edupdp/edu10.svg";
+
+const approach = [
+  {
+    approach_title: "Sertifikat bilan taqdirlash",
+    approach_desc:
+      "Kurs so'nggida talabaning o'zlashtirish ko'rsatkichlari e'tiborga olingan holda sertifikat va diplom beriladi.",
+    approach_img: edu7,
+  },
+  {
+    approach_title: "O’quv jarayoni",
+    approach_desc:
+      "O'quv jarayoni video darslar, amaliy mashqlar, mentor bilan ishlash imkoniyati hamda live meetinglar orqali olib boriladi.",
+    approach_img: edu8,
+  },
+  {
+    approach_title: "Real loyihalarni amalga oshirish",
+    approach_desc:
+      "Har bir kursdagi mavzulashtirilgan video darslarda real loyihalardan qismlar orqali tushuntirib beriladi.",
+    approach_img: edu5,
+  },
+  {
+    approach_title: "Sertifikat bilan taqdirlash",
+    approach_desc:
+      "Kurs so'nggida talabaning o'zlashtirish ko'rsatkichlari e'tiborga olingan holda sertifikat va diplom beriladi.",
+    approach_img: edu9,
+  },
+  {
+    approach_title: "Ish bilan ta’minlashda amaliy ko’mak",
+    approach_desc:
+      "Kurslarni muvaffaqiyatli tamomlagan talabalarga PDP akademiyasi tomonidan ish bilan ta’minlashda amaliy yordam ko’rsatiladi.",
+    approach_img: edu6,
+  },
+  {
+    approach_title: "Ishga kiring va karyerangizni boshlang",
+    approach_desc:
+      "Biz bilan kurslarni muvaffaqiyatli tamomlang va orzu qilgan ish o'rningiz egasiga aylaning.",
+    approach_img: edu10,
+  },
+];
---
+++ src/API/mentor.jsx
@@ -0,0 +1,75 @@
+// edu
+
+import ecma from "../assets/pdpApiimagesfile/download-5.png";
+import pdp from "../assets/pdpApiimagesfile/download-7.png";
+import dispatch from "../assets/pdpApiimagesfile/download-6.png";
+
+// avatar
+
+import avatar1 from "../assets/pdpApiimagesfile/download-11.png";
+import avatar2 from "../assets/pdpApiimagesfile/download-5.png";
+import avatar3 from "../assets/pdpApiimagesfile/download.png";
+import avatar4 from "../assets/pdpApiimagesfile/download-7.png";
+import avatar5 from "../assets/pdpApiimagesfile/download-6.png";
+import avatar6 from "../assets/pdpApiimagesfile/download-4.png";
+import avatar7 from "../assets/pdpApiimagesfile/download-2.png";
+import avatar8 from "../assets/pdpApiimagesfile/download-3.png";
+
+const mentor = [
+  {
+    id: 1,
+    mentor_avatar: avatar1,
+    mentor_name: "Dilshod Mirsoatov",
+    mentor_job: "Senior Frontend Developer",
+    mentor_edu: ecma,
+  },
+  {
+    id: 2,
+    mentor_avatar: avatar2,
+    mentor_name: "Khurshidbek Kurbanov",
+    mentor_job: "Senior Mobile Developer",
+    mentor_edu: pdp,
+  },
+  {
+    id: 3,
+    mentor_avatar: avatar3,
+    mentor_name: "Azimjon Po'latov",
+    mentor_job: "Software Engineer",
+    mentor_edu: dispatch,
+  },
+  {
+    id: 4,
+    mentor_avatar: avatar4,
+    mentor_name: "Alisher Davlatov",
+    mentor_job: "Senior Backend Developer",
+    mentor_edu: ecma,
+  },
+  {
+    id: 5,
+    mentor_avatar: avatar5,
+    mentor_name: "Sirojiddin Saidov",
+    mentor_job: "Spring Developer",
+    mentor_edu: ecma,
+  },
+  {
+    id: 6,
+    mentor_avatar: avatar6,
+    mentor_name: "Sanjar Suvonov",
+    mentor_job: "Android Developer",
+    mentor_edu: ecma,
+  },
+  {
+    id: 7,
+    mentor_avatar: avatar7,
+    mentor_name: "Abdusobur Xalimov",
+    mentor_job: "Fullstack Web developer",
+    mentor_edu: ecma,
+  },
+  {
+    id: 8,
+    mentor_avatar: avatar8,
+    mentor_name: "Nurbek Boboyev",
+    mentor_job: "Mentor",
+    mentor_edu: pdp,
+  },
+];
