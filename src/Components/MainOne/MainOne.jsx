da6030aa7ff480565d80c6dacc19b68710406772
alimardon28 <doniyorovalimardon369@gmail.com>
2022-09-12 20:56:09 +0500

    ok

--- src/Components/MainOne/MainOne.jsx
+++ src/Components/MainOne/MainOne.jsx
@@ -5,6 +5,8 @@ import video from "../../assets/pdpicons/play-video.svg";
 import hq from "../../assets/images/hqdefault.jpg";
 
 const MainOne = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className="mainone">
       <div className="container">
@@ -18,34 +20,39 @@ const MainOne = () => {
             <img className="mainone__ul_li_homeimg" src={bg} alt="" />
           </li>
           <li className="mainone__ul_li">
-            <button className="mainone__ul_li_box">
+            <button
+              className="mainone__ul_li_box"
+              onClick={() => setOpenModal(!openModal)}
+            >
               <img
                 className="mainone__ul_li_videoimg mainone__ul_li_img"
                 src={video}
                 alt=""
               />
             </button>
-            {/* <div className="mainone__ul_li_dialog">
-              <div className="mainone__ul_li_dialog_box">
-                <iframe
-                  className="mainone__ul_li_dialog_box_iframe"
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/IlVpkBf3McU"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                ></iframe>
 
+            {openModal && (
+              <div className="mainone__ul_li_dialog">
+                <div className="mainone__ul_li_dialog_box">
+                  <iframe
+                    className="mainone__ul_li_dialog_box_iframe"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/IlVpkBf3McU"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+                <button
+                  className="mainone__ul_li_dialog_btn"
+                  onClick={() => setOpenModal(false)}
+                >
+                  x
+                </button>
               </div>
-              <button
-                className="mainone__ul_li_dialog_btn"
-                onClick={() => setOpenModal(false)}
-              >
-                x
-              </button>
-            </div> */}
+            )}
           </li>
         </ul>
       </div>
